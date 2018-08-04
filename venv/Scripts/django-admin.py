f5a9251d88c5bc4ee851727cf73b381fdcdb72d5
Paulo Rezende <rezendepauloh@gmail.com>
2018-08-03 23:52:05 -0400

    Primeiro commit: Importando o venv com scikit-learn, numpy, scipy, matplotlib, seaborn e django

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Dropbox\Documentos\Programação\Python\MonitoramentoBovino\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
